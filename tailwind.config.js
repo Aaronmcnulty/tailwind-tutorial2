916a8d8b297f0c00cdbc1c3991a8672a9ae882e3
malcomcrab <malcomcrabesq@outlook.com>
2025-04-14 16:28:56 +0100

    Style: Adds mobile menu to index.html and keyframe animation tp tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const { transform } = require('lightningcss');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./build/*html'],
@@ -6,6 +8,16 @@ module.exports = {
       screens:{
         'widescreen': {'raw': '(min-aspect-ratio: 3/2)'},
         'tallscreen': {'raw': '(min-aspect-ratio: 13/20)'}
+      }, 
+      keyframes: {
+        'open-menu' :{
+          '0%': { transform: 'scaleY(0)' },
+          '80%': {transform: 'scaleY(1,2)'},
+          '100%': {transform: 'scaleY(1)'},
+        },
+      },
+      animation: {
+        'open-menu': 'open-menu 0.5 ease-in-out forwards',
       }
     },
   },
